8af1f35e29ec0b7a6d7ece1a2d7b7ef74ed9aab7
Alberto Bietti <alberto.bietti@gmail.com>
2013-02-16 18:49:45 -0800

    support hands + fingers

--- src/translator.js
+++ src/translator.js
@@ -1,72 +1,96 @@
+
+function defaultPool(size) {
+  var pool = [];
+  for (var i = size-1; i >= 0; i--) {
+    pool.push(i);
+  }
+  return pool;
+}
+
 var handOn = false;
+var hIdxPoolSize = 2;
+var hIdxPool = defaultPool(hIdxPoolSize);
+var hToIdx = [];
 
 function handMessages(json) {
   var messages = [];
   if (!json.hasOwnProperty('hands')) {
     return messages;
   }
-  newHandOn = json.hands.length > 0;
-  if (handOn && newHandOn) {
-    messages.push({address: "/hand/val", value: json.hands[0]['palmPosition']});
-  } else if (handOn) {
-    messages.push({address: "/hand/on", value: [0]});
-  } else if (newHandOn) {
-    messages.push({address: "/hand/on", value: [1]});
-    messages.push({address: "/hand/val", value: json.hands[0]['palmPosition']});
+  newHandOn = [];
+  hands = {};
+  for (i in json.hands) {
+    h = json.hands[i];
+    newHandOn[h.id] = true;
+    hands[h['id']] = h['palmPosition'];
+  }
+  for (var i = 0; i < Math.max(handOn.length, newHandOn.length); i++) {
+    if (handOn[i] && newHandOn[i]) {
+      var idx = hToIdx[i];
+      messages.push({address: "/hand/" + idx + "/val", value: hands[i]});
+    } else if (handOn[i]) {
+      var idx = hToIdx[i];
+      messages.push({address: "/hand/" + idx + "/on", value: [0]});
+      hToIdx[i] = undefined;
+      hIdxPool.push(idx);
+
+      // reset pool when no hands are on
+      if (hIdxPool.length === hIdxPoolSize) {
+        hIdxPool = defaultPool(hIdxPoolSize);
+      }
+    } else if (newHandOn[i]) {
+      var idx = hIdxPool.pop();
+      hToIdx[i] = idx;
+      messages.push({address: "/hand/" + idx + "/on", value: [1]});
+      messages.push({address: "/hand/" + idx + "/val", value: hands[i]});
+    }
   }
   handOn = newHandOn;
   return messages;
 }
 
+var fingerOn = [];
+var fIdxPoolSize = 8;
+var fIdxPool = defaultPool(fIdxPoolSize);
+var fToIdx = [];
 
-var idxPoolSize = 8;
-var pointableOn = [];
-var idxPool = defaultPool();
-var pToIdx = [];
-
-function defaultPool() {
-  var pool = [];
-  for (var i = idxPoolSize-1; i >= 0; i--) {
-    pool.push(i);
-  }
-  return pool;
-}
-
-function pointableMessages(json) {
+function fingerMessages(json) {
   var messages = [];
   if (!json.hasOwnProperty('hands')) {
     return messages;
   }
-  newPointableOn = [];
-  pointables = {};
+  newFingerOn = [];
+  fingers = {};
   for (i in json.pointables) {
-    p = json.pointables[i];
-    newPointableOn[p.id] = true;
-    pointables[p['id']] = p['tipPosition'];
+    f = json.pointables[i];
+    newFingerOn[f.id] = true;
+    fingers[f['id']] = f['tipPosition'];
   }
-  for (var i = 0; i < Math.max(pointableOn.length, newPointableOn.length); i++) {
-    if (pointableOn[i] && newPointableOn[i]) {
-      var idx = pToIdx[i];
-      messages.push({address: "/pointable/" + idx + "/val", value: pointables[i]});
-    } else if (pointableOn[i]) {
-      var idx = pToIdx[i];
-      messages.push({address: "/pointable/" + idx + "/on", value: [0]});
-      pToIdx[i] = undefined;
-      idxPool.push(idx);
+  for (var i = 0; i < Math.max(fingerOn.length, newFingerOn.length); i++) {
+    if (fingerOn[i] && newFingerOn[i]) {
+      var idx = fToIdx[i];
+      messages.push({address: "/finger/" + idx + "/val", value: fingers[i]});
+    } else if (fingerOn[i]) {
+      var idx = fToIdx[i];
+      messages.push({address: "/finger/" + idx + "/on", value: [0]});
+      fToIdx[i] = undefined;
+      fIdxPool.push(idx);
 
-      // reset pool when no pointables are on
-      if (idxPool.length === idxPoolSize) {
-        idxPool = defaultPool();
+      // reset pool when no fingers are on
+      if (fIdxPool.length === fIdxPoolSize) {
+        fIdxPool = defaultPool(fIdxPoolSize);
       }
-    } else if (newPointableOn[i]) {
-      var idx = idxPool.pop();
-      pToIdx[i] = idx;
-      messages.push({address: "/pointable/" + idx + "/on", value: [1]});
-      messages.push({address: "/pointable/" + idx + "/val", value: pointables[i]});
+    } else if (newFingerOn[i]) {
+      var idx = fIdxPool.pop();
+      fToIdx[i] = idx;
+      messages.push({address: "/finger/" + idx + "/on", value: [1]});
+      messages.push({address: "/finger/" + idx + "/val", value: fingers[i]});
     }
   }
-  pointableOn = newPointableOn;
+  fingerOn = newFingerOn;
   return messages;
-};
+}
 
-module.exports = pointableMessages;
+module.exports = function(json) {
+  return handMessages(json).concat(fingerMessages(json));
+}
