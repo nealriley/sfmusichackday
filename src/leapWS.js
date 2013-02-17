d791abfd8716bc796b72856b6cdc40863496b531
this guy <dean@ero.com>
2013-02-16 18:53:14 -0800

    Add some web scaffolding

---
+++ src/leapWS.js
@@ -0,0 +1,40 @@
+module.exports = {
+  start: function () {
+    console.log("in leapWS");
+    var ws;
+
+    // Support both the WebSocket and MozWebSocket objects
+    if ((typeof(WebSocket) == 'undefined') &&
+        (typeof(MozWebSocket) != 'undefined')) {
+      WebSocket = MozWebSocket;
+    }
+    //Create and open the socket
+    ws = new WebSocket("ws://localhost:6437/");
+    
+    // On successful connection
+    ws.onopen = function(event) {
+      document.getElementById("main").style.visibility = "visible";
+      document.getElementById("connection").innerHTML = "WebSocket connection open!";
+    };
+    
+    // On message received
+    ws.onmessage = function(event) {
+      var obj = JSON.parse(event.data);
+      var str = JSON.stringify(obj, undefined, 2);
+      window.data = str;
+      document.getElementById("output").innerHTML = '<pre>' + str + '</pre>';
+    };
+    
+    // On socket close
+    ws.onclose = function(event) {
+      ws = null;
+      document.getElementById("main").style.visibility = "hidden";
+      document.getElementById("connection").innerHTML = "WebSocket connection closed";
+    }
+    
+    //On socket error
+    ws.onerror = function(event) {
+      alert("Received error");
+    };
+  }
+};
---
+++ src/viz.coffee
@@ -0,0 +1,6 @@
+class Viz
+  @start: (canvasElement) ->
+    console.log "IN VIS!"
+
+module.exports = Viz
+
