5210f6876192a1447bd286e1cd938195e95d2725
Alberto Bietti <alberto.bietti@gmail.com>
2013-02-16 16:53:54 -0800

    osc translator: send different messages for pointables (on + vals)

--- src/translator.js
+++ src/translator.js
@@ -1,12 +1,27 @@
+var pointableOn = [];
+
 module.exports = function (json) {
   var messages = [];
-  if (json["hands"]) {
-    var arg  = "Hello?";
-    var hand = null;
-    if (hand = json.hands[0]) {
-      messages.push({address: "/hand/" + hand["id"], value: arg}); 
+  if (!json.hands) {
+    return messages;
+  }
+  newPointableOn = [];
+  pointables = {};
+  for (i in json.pointables) {
+    p = json.pointables[i];
+    newPointableOn[p.id] = true;
+    pointables[p['id']] = p['tipPosition'];
+  }
+  for (var i = 0; i < Math.max(pointableOn.length, newPointableOn.length); i++) {
+    if (pointableOn[i] && newPointableOn[i]) {
+      messages.push({address: "/pointable/" + i + "/val", value: pointables[i]});
+    } else if (pointableOn[i]) {
+      messages.push({address: "/pointable/" + i + "/on", value: 0});
+    } else if (newPointableOn[i]) {
+      messages.push({address: "/pointable/" + i + "/on", value: 1});
+      messages.push({address: "/pointable/" + i + "/val", value: pointables[i]});
     }
   }
-
+  pointableOn = newPointableOn;
   return messages;
 };
